bd9f9f03f6c6b7e9e90e8c2ad3a1fae67fed4297
asd <31347791+Youssef-Mekkkawy@users.noreply.github.com>
2026-03-23 22:50:22 +0200

    Use Vite for login page assets
    Replace the Blade asset() stylesheet link with a @vite directive to load compiled CSS and JS for the login page. The original "<link rel=\"stylesheet\" href=\"{{ asset('css/login.css') }}\">" line is commented out for reference. This integrates the login view with Vite's dev server and production bundling (HMR, cache-busting).

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,8 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Sora:wght@300;400;500;600;700;800&family=DM+Sans:wght@300;400;500;600&display=swap"
         rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('css/login.css') }}"> --}}
+    @vite(['resources/css/login.css', 'resources/js/login.js'])
 </head>
 
 <body>
